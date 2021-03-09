fece28740bd88b1a4414c1c2071983af9b205ce3
Resonant sol <lms@resonantstyle.com>
2021-03-09 13:23:14 +0000

    Add ja lang

---
+++ lang/ja/block_course_search.php
@@ -0,0 +1,40 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_course_search', language 'ja'
+ *
+ * @package   block_course_search
+ * @copyright 2021 Resonant Solutions
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['course_search:addinstance'] = '新しいコース活動検索ブロックを追加する';
+$string['course_search:myaddinstance'] = '新しいコース活動検索ブロックを追加する';
+$string['configmodulestosearch'] = '検索対象の活動モジュールをカンマ区切りで記述する';
+$string['configresultwithsummary'] = '検索結果に活動の概要を表示する';
+$string['modulestosearch'] = '検索対象の活動モジュール';
+$string['new_search'] = '再検索';
+$string['no_result'] = 'No result';
+$string['pagetitle'] = 'コース活動検索結果';
+$string['pluginname'] = 'コース活動検索';
+$string['results'] = '検索結果 :';
+$string['resultwithsummary'] = '概要を表示する';
+$string['return_course'] = 'コースへ戻る';
+$string['searchfor'] = 'キーワード';
+$string['settings'] = '設定';
+$string['submit'] = 'Go';
